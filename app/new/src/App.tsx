9ebfe40514e1a53d66a01217f1a2d9e66416255a
Kayode96-max <olukayodeusman406@gmail.com>
2026-01-14 14:08:22 +0100

    feat: create App component with ChessBoard integration

---
+++ app/new/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ChessBoard from './ChessBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chess App</h1>
+      <ChessBoard />
+    </div>
+  );
+}
+
+export default App;
