cefed67ccdfdece877a5f8669603d855511e0936
Kayode96-max <olukayodeusman406@gmail.com>
2026-01-19 15:33:17 +0100

    Add TransactionsPage component to display transaction history

---
+++ public/New/src/app/transactions/page.tsx
@@ -0,0 +1,9 @@
+import { TransactionHistory } from '@/components/transactions/TransactionHistory';
+
+export default function TransactionsPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <TransactionHistory />
+    </div>
+  );
+}
